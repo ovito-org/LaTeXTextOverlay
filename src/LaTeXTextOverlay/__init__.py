b8d8dfa8ed3552d9c23d4012a5e26660a571ee43
Daniel Utt <utt@ovito.org>
2024-02-09 12:16:51 +0100

    Initial commit

---
+++ src/LaTeXTextOverlay/__init__.py
@@ -0,0 +1,45 @@
+#### LaTeX Text Overlay ####
+# Render LaTeX text in the viewport
+
+import ovito
+from matplotlib.rcsetup import _validators
+from ovito.data import DataCollection
+from ovito.traits import Color
+from ovito.vis import ViewportOverlayInterface
+from traits.api import Code, Enum, Range
+
+
+class LaTeXTextOverlay(ViewportOverlayInterface):
+    text = Code(value="\exp^{i \pi} + 1 = 0", label="Text")
+    font = Enum(_validators["mathtext.fontset"].valid.values(), label="Font")
+    fontsize = Range(low=1, high=None, value=50, label="Font size")
+    if ovito.version >= (3, 10, 3):
+        text_color = Color(default=(0.0, 0.0, 0.0), label="Text color")
+    else:
+        text_color = (0.0, 0.0, 0.0)
+    px = Range(low=0.0, high=1.0, value=0.5, label="X position")
+    py = Range(low=0.0, high=1.0, value=0.5, label="Y position")
+
+    def render(
+        self, canvas: ViewportOverlayInterface.Canvas, data: DataCollection, **kwargs
+    ):
+        with canvas.mpl_figure(
+            pos=(self.px - 0.5, 0.5 + self.py),
+            size=(1.0, 1.0),
+            anchor="north west",
+            alpha=0,
+            tight_layout=True,
+        ) as fig:
+            ax = fig.subplots()
+            if self.text:
+                ax.text(
+                    0.5,
+                    0.5,
+                    f"${self.text}$",
+                    fontsize=self.fontsize,
+                    horizontalalignment="center",
+                    verticalalignment="center",
+                    color=[*self.text_color, 1.0],
+                    math_fontfamily=self.font,
+                )
+            ax.axis("off")
