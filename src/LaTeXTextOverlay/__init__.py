3d0a3df01071f8c4a611201eb7f60d296df2ebea
Daniel Utt <utt@ovito.org>
2024-03-06 09:58:23 +0100

    Add support for background color patch

--- src/LaTeXTextOverlay/__init__.py
+++ src/LaTeXTextOverlay/__init__.py
@@ -6,7 +6,7 @@ from matplotlib.rcsetup import _validators
 from ovito.data import DataCollection
 from ovito.traits import Color
 from ovito.vis import ViewportOverlayInterface
-from traits.api import Code, Enum, Range
+from traits.api import Bool, Code, Enum, Range
 
 
 class LaTeXTextOverlay(ViewportOverlayInterface):
@@ -19,10 +19,22 @@ class LaTeXTextOverlay(ViewportOverlayInterface):
         text_color = (0.0, 0.0, 0.0)
     px = Range(low=0.0, high=1.0, value=0.5, label="X position")
     py = Range(low=0.0, high=1.0, value=0.5, label="Y position")
+    if ovito.version >= (3, 10, 3):
+        show_background = Bool(False, label="Show background")
+        background_color = Color(default=(1.0, 0.5, 0.5), label="Background color")
 
     def render(
         self, canvas: ViewportOverlayInterface.Canvas, data: DataCollection, **kwargs
     ):
+        if ovito.version >= (3, 10, 3) and self.show_background:
+            bbox = dict(
+                boxstyle="round",
+                ec=self.background_color,
+                fc=self.background_color,
+            )
+        else:
+            bbox = None
+
         with canvas.mpl_figure(
             pos=(self.px - 0.5, 0.5 + self.py),
             size=(1.0, 1.0),
@@ -41,5 +53,6 @@ class LaTeXTextOverlay(ViewportOverlayInterface):
                     verticalalignment="center",
                     color=[*self.text_color, 1.0],
                     math_fontfamily=self.font,
+                    bbox=bbox,
                 )
             ax.axis("off")
